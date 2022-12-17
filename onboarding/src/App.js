103ffa55f43715251b006f359be94318dbbc9808
Sabrina <Smg21022@gmail.com>
2022-12-17 16:48:45 -0700

    fixing spelling

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -2,6 +2,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from './Components/Form.js';
+import schema from './Components/Validation/formSchema.js';
+import * as yup from 'yup';
+
 
 const initialFormValues = {
   usedrname: '',
@@ -9,19 +12,40 @@ const initialFormValues = {
   email: '',
   tos: false
 }
+
+const initialFormErrors = {
+  usedrname: '',
+  password: '',
+  email: '',
+  tos: ''
+}
+
+
 function App() {
   const [formValues, setFormValues] = useState(initialFormValues);
+  
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  
+  
   const handleSubmit = () => {
     // WIP
   }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+    .validate(value)
+    .then(()=> setFormErrors({...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({...formErrors, [name]: err.errors[0] }))
+  }
   
   const handleChange = (name, value) => {
+    validate(name, value);
     setFormValues({...formValues, [name]: value});
   }
 
   return (
     <div className="App">
-      < Form values={formValues} change={handleChange} />
+      < Form values={formValues} change={handleChange} errors={formErrors} />
     </div>
   );
 }
--- onboarding/src/Components/Form.js
+++ onboarding/src/Components/Form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Form = (props) => {
-    const { change, submit } = props;
-    const { username, email, password, checked } = props.values;
+    const { change, submit, errors } = props;
+    const { username, email, password, tos } = props.values;
 
     const onChange = (e) => {
         const  { name, value, checked, type }= e.target;
@@ -18,6 +18,10 @@ const Form = (props) => {
     return (
     <div>
         <h1>My cool form!</h1>
+        <p>{errors.username}</p>
+        <p>{errors.password}</p>
+        <p>{errors.email}</p>
+        <p>{errors.tos}</p>
         <form onSubmit={onSubmit} >
             <label>
                 Name:
@@ -60,7 +64,7 @@ const Form = (props) => {
                 <input
                 type="submit"
                 name="Create A Friend!"
-                value="{name}"
+                value="Create A Friend!"
                 onChange={onChange}
                 />
             </label>
